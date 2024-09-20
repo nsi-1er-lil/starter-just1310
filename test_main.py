427e371a8745974d69e23c1111c1777ab02c4d2c
just1310 <justined@lyceechurchill.london>
2024-09-20 11:32:27 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,3 +1,7 @@
-git add test_main.py
-git commit -m "Add test file"
-git push
+import pytest
+from main import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
