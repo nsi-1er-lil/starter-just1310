d208a8a4d735ae886a9a4d76a3c8fed1a4eb7429
just1310 <justined@lyceechurchill.london>
2024-10-01 11:45:48 +0100

    Instructions_conditionnelles

---
+++ Instructions_conditionnelles/exercice_11.py
@@ -0,0 +1,8 @@
+def starter(pokemon_choisi):
+    if pokemon_choisi == "Bulbizarre":
+        pokemon_rival = "Carapuce"
+    elif pokemon_choisi == "Carapuce":
+        pokemon_rival = "Salamèche"
+    else:
+        pokemon_rival = "Bulbizarre"
+    return pokemon_rival
